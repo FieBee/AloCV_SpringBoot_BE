dcaa53ec8c7fc58e5dcc2795e74d015a21330d9d
ishopjapan <thoitran2107@gmail.com>
2022-11-02 17:06:02 +0700

    lam controller jobfield va location

---
+++ src/main/java/com/example/alocv_be/service/jobField/IJobFieldService.java
@@ -0,0 +1,8 @@
+package com.example.alocv_be.service.jobField;
+
+import com.example.alocv_be.model.entity.JobField;
+import com.example.alocv_be.service.IGerenalService;
+
+public interface IJobFieldService extends IGerenalService<JobField> {
+
+}
---
+++ src/main/java/com/example/alocv_be/service/location/LocationService.java
@@ -0,0 +1,34 @@
+package com.example.alocv_be.service.location;
+
+import com.example.alocv_be.model.entity.Location;
+import com.example.alocv_be.repo.LocationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LocationService implements ILocationService{
+    @Autowired
+    private LocationRepo locationRepo;
+
+    @Override
+    public Iterable<Location> findAll() {
+        return locationRepo.findAll();
+    }
+
+    @Override
+    public Optional<Location> findById(Long id) {
+        return locationRepo.findById(id);
+    }
+
+    @Override
+    public Location save(Location location) {
+        return locationRepo.save(location);
+    }
+
+    @Override
+    public void remove(Long id) {
+        locationRepo.deleteById(id);
+    }
+}
