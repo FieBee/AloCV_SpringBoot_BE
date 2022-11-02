a0997e8ed76c2c3ab8287d6b37ec15aad027ee39
ishopjapan <thoitran2107@gmail.com>
2022-11-02 17:46:22 +0700

    updatte controller

---
+++ src/main/java/com/example/alocv_be/controller/NotificationController.java
@@ -0,0 +1,59 @@
+package com.example.alocv_be.controller;
+
+import com.example.alocv_be.model.entity.CV;
+import com.example.alocv_be.model.entity.Notification;
+import com.example.alocv_be.service.cv.ICVService;
+import com.example.alocv_be.service.notification.INotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/notifications")
+public class NotificationController {
+    @Autowired
+    private INotificationService notificationService;
+    @GetMapping
+    public ResponseEntity<Iterable<Notification>> findAllNotification(){
+        List<Notification> notifications = (List<Notification>) notificationService.findAll();
+        if (notifications.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(notifications, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Notification> findNotificationById(@PathVariable Long id){
+        Optional<Notification> notificationOptional = notificationService.findById(id);
+        if (!notificationOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(notificationOptional.get(), HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Notification> saveNotification(@RequestBody Notification notification){
+        return new ResponseEntity<>(notificationService.save(notification), HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Notification> updateNotification(@PathVariable Long id, @RequestBody Notification notification){
+        Optional<Notification> notificationOptional = notificationService.findById(id);
+        if (!notificationOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        notification.setId(notificationOptional.get().getId());
+        return new ResponseEntity<>(notificationService.save(notification), HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Notification> deleteNotification(@PathVariable Long id){
+        Optional<Notification> notificationOptional = notificationService.findById(id);
+        if (!notificationOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        notificationService.remove(id);
+        return new ResponseEntity<>(notificationOptional.get(), HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/example/alocv_be/service/notification/INotificationService.java
@@ -0,0 +1,9 @@
+package com.example.alocv_be.service.notification;
+
+import com.example.alocv_be.model.entity.Notification;
+import com.example.alocv_be.service.IGerenalService;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface INotificationService extends IGerenalService<Notification> {
+}
