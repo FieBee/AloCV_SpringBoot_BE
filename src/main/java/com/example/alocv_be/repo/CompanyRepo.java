abb8efde46e3601fcf288c7b168d1a3df5d369ab
AnhDung95 <nguyendung2172013@gmail.com>
2022-11-03 08:32:16 +0700

    fix crud company lan 2

--- src/main/java/com/example/alocv_be/repo/CompanyRepo.java
+++ src/main/java/com/example/alocv_be/repo/CompanyRepo.java
@@ -2,6 +2,8 @@ package com.example.alocv_be.repo;
 
 import com.example.alocv_be.model.entity.Company;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepo extends PagingAndSortingRepository<Company, Long> {
 }
