64b308cbabedab7aeeae1c2304f160733149313a
Dao Van Trong <daovantrong98@gmail.com>
2022-11-03 10:04:38 +0700

    them sua xoa job

--- src/main/java/com/example/alocv_be/repo/IJobRepo.java
+++ src/main/java/com/example/alocv_be/repo/IJobRepo.java
@@ -3,6 +3,8 @@ package com.example.alocv_be.repo;
 import com.example.alocv_be.model.entity.Account;
 import com.example.alocv_be.model.entity.Job;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IJobRepo extends PagingAndSortingRepository<Job, Long> {
 }
