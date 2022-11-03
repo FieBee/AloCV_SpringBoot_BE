145499516085147c0c50c24a0b53a08c61906f8b
FieBee <phuonglf98@gmail.com>
2022-11-03 09:29:10 +0700

    Thêm phương thức lấy ra company từ Account_UserName

--- src/main/java/com/example/alocv_be/service/company/ICompanyService.java
+++ src/main/java/com/example/alocv_be/service/company/ICompanyService.java
@@ -1,7 +1,11 @@
 package com.example.alocv_be.service.company;
 
 import com.example.alocv_be.model.entity.Company;
+import com.example.alocv_be.model.entity.User;
 import com.example.alocv_be.service.IGerenalService;
 
+import java.util.Optional;
+
 public interface ICompanyService extends IGerenalService<Company> {
+    Optional<User> findCompanyByAccount_UserName(String userName);
 }
