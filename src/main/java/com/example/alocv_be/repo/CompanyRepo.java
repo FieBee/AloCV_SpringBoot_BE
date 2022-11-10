1f373089a70df0a04972e11f7210dd940a20788a
ishopjapan <thoitran2107@gmail.com>
2022-11-10 22:08:58 +0700

    hihi

--- src/main/java/com/example/alocv_be/repo/CompanyRepo.java
+++ src/main/java/com/example/alocv_be/repo/CompanyRepo.java
@@ -1,9 +1,11 @@
 package com.example.alocv_be.repo;
 
+import com.example.alocv_be.model.CV;
 import com.example.alocv_be.model.Company;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
