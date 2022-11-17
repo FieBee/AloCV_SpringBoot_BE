6b132401a500704b9dab71c93c03f03239e939db
FieBee <phuonglf98@gmail.com>
2022-11-17 10:36:58 +0700

    edit SecurityConfig

--- src/main/java/com/example/alocv_be/config/SecurityConfig.java
+++ src/main/java/com/example/alocv_be/config/SecurityConfig.java
@@ -44,11 +44,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("**").permitAll()
-//                .and()
-//                .authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN").and()
-//                .authorizeRequests().antMatchers("/company/**").hasRole("COMPANY").and()
-//                .authorizeRequests().antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/login","/register/**","/").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/admin/**","/user/**","/company/**").hasRole("ADMIN").and()
+                .authorizeRequests().antMatchers("/company/**").hasRole("COMPANY").and()
+                .authorizeRequests().antMatchers("/user/**").hasRole("USER")
                 .and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
         http.csrf().disable();
